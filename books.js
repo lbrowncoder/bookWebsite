833fd6a5fcbf0c8a73ea63aa4fe538beb0e1b174
marab <69154212+lbrowncoder@users.noreply.github.com>
2020-09-24 23:53:21 +0100

    second

--- books.js
+++ books.js
@@ -1,64 +1,77 @@
-document.querySelector("displayBooks").appendChild(titleElement);
-
-let authorElement = document.createElement("h2");
-let authorText = document.createTextNode(item.Author);
-authorElement.appendChild(authorText);
-document.querySelector("#displayBooks").appendChild(authorElement);
-
-let spanElement = document.createElement("span");
-let priceText = document.createTextNode(item.Price);
-authorElement.appendChild(PriceText);
-document.querySelector("#displayBooks").appendChild(priceElement);
-
-let nameElement = document.createElement("name");
-let nameText = document.createTextNode(item.Name);
-authorElement.appendChild(nameText);
-document.querySelector("#displayBooks").appendChild(nameElement);
-
-let descriptionElement = document.createElement("Description");
-let descriptionText = document.createTextNode(item.Description);
-authorElement.appendChild(descriptionText);
-document.querySelector("#displayBooks").appendChild(descriptionElement);
-
-let imgElement = document.createElement("img");
-let imgText = document.createTextNode(img.author);
-authorElement.appendChild(imgText);
-document.querySelector("#displayBooks").appendChild(imgElement);
-imgElement.height = 350;
-imgElement.width = 350;
-
-document.querySelector("#displayBooks").appendChild(imgElement)
-  
 let mybookshop = [
     {
-    Author: "Saeed Jones",
-    Name: "How We Fight for Our Lives",
-    Price: "£12.99",
-    Description: "Jones's bestselling memoir is a personal account of growing up in the South as a young gay man who's attempting to find himself while battling rocky relationships with family, friends, and lovers.",
-    /*img scr = ".howWeFght.jpg";*/
+    author: "Saeed Jones",
+    name: "How We Fight for Our Lives",
+    price: "£12.99",
+    description: "Jones's bestselling memoir is a personal account of growing up in the South as a young gay man who's attempting to find himself while battling rocky relationships with family, friends, and lovers.",
+    photo: "./images",
     },
     {
-    Author: "Jesmyn Ward",
-    Name: "Salvage the Bones",
-    Price: "£9.99",
-    Description: "A 2011 National Book Award Winner, Salvage the Bones chronicles a 12-day period in which a poor Mississippi family faces the looming threat of a hurricane. With no mother, a drunken father, a pregnant 14-year-old, and wayward brothers, they have little access to helpful resources—except for love.",
-    /*img.scr = "bones.jpg"*/
+    author: "Jesmyn Ward",
+    name: "Salvage the Bones",
+    price: "£9.99",
+    description: "A 2011 National Book Award Winner, Salvage the Bones chronicles a 12-day period in which a poor Mississippi family faces the looming threat of a hurricane. With no mother, a drunken father, a pregnant 14-year-old, and wayward brothers, they have little access to helpful resources—except for love.",
+    photo: "./images/bones.jpg",
 },
     {
-    Author: "Maya Angelou",
-    Name: "I Know Why the Caged Bird Sings",
-    Price: "£11.99",
-    Description: "Written by a legendary writer, civil rights activist, and one of Oprah's greatest friends, I Know Why the Caged Bird Sings is an poetic memoir that captures Angelou's childhood struggles and the freedoms of her adulthood, which allowed her to find strength amidst despair.",
-    /*img.scr = "bird.jpg"*/    
+    author: "Maya Angelou",
+    name: "I Know Why the Caged Bird Sings",
+    price: "£11.99",
+    description: "Written by a legendary writer, civil rights activist, and one of Oprah's greatest friends, I Know Why the Caged Bird Sings is an poetic memoir that captures Angelou's childhood struggles and the freedoms of her adulthood, which allowed her to find strength amidst despair.",
+    photo: "./images/bird.jpg",    
     },
     {
-    Author: "Akwaeke Emezi",
-    Name: "Freshwater",
-    Price: "8.99",
-    Description: "In this debut for Emezi, we meet Ada, a young Nigerian woman who's born with multiple personalities (or spirits). As the story goes on, we hear from each, as they drive Ada towards reckless behavior as she grows up. But once she moves to America and endures a traumatic event, the spirits threaten to takeover.",
-    /*img.scr = "water.jpg"*/    
+    author: "Akwaeke Emezi",
+    name: "Freshwater",
+    price: "8.99",
+    description: "In this debut for Emezi, we meet Ada, a young Nigerian woman who's born with multiple personalities (or spirits). As the story goes on, we hear from each, as they drive Ada towards reckless behavior as she grows up. But once she moves to America and endures a traumatic event, the spirits threaten to takeover.",
+    photo: "./images/water.jpg",
     },
-]
-/*for (let i = 0; i < mybookshop.length; i++){
-    let document,createElement("h2");
-    document.createTextNode(mybookshop[i].Name);*/
+];
+
+for (let i = 0; i < mybookshop.length; i++){
+
+    let bookDiv = document.createElement("div"); // Create entire div for each book
+
+    // book name
+    let nameEl = document.createElement("h2");
+    let bookname = document.createTextNode(mybookshop[i].name);
+    nameEl.appendChild(bookname);
+
+    // book author
+    let authorEl = document.createElement("p");
+    let bookauthor = document.createTextNode(`Author: ${mybookshop[i].author}`);
+    authorEl.appendChild(bookauthor);
+
+    // price
+    let priceEl = document.createElement("p");
+    let bookprice = document.createTextNode(`Price: ${mybookshop[i].price}`);
+    priceEl.appendChild(bookprice);
+    console.log(bookprice);
+
+     // description
+    let descriptionEl = document.createElement("p");
+    let bookdescription = document.createTextNode(`${mybookshop[i].description}`);
+    descriptionEl.appendChild(bookdescription);
+
+    // image
+    let imageEl = document.createElement("img");
+    let bookphoto = document.getElementById(`${mybookshop[i].photo}`);
+    imageEl.appendChild(bookphoto);
+    imageEl.height = 250;
+    imageEl.width = 250;
+    console.log(imageEl),
+
+
+
+
+    // appending each part of book to book div (order depends on order here)
+    bookDiv.appendChild(nameEl);
+    bookDiv.appendChild(authorEl); 
+    bookDiv.appendChild(priceEl);
+    bookDiv.appendChild(bookdescription);
+    bookDiv.appendChild(imageEl);
+
+    // appends the div to the entire document
+    document.getElementById("books").appendChild(bookDiv);
+}  
